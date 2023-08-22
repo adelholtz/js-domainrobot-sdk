46f5786bdf86a027a79b0d6172740ec6305d8c59
Benjamin Krammel <bk@internetx.de>
2023-08-22 14:17:55 +0200

    DEBUG

--- src/services/DomainRobotService.js
+++ src/services/DomainRobotService.js
@@ -135,6 +135,7 @@ class DomainRobotService {
 
             return domainRobotResult;
         } catch (error) {
+            console.log(error)
             throw new DomainRobotException(
                 error.response.data,
                 error.response.status
